1d9cb251eafcde7cb8e18869ba10128606b26314
Stijn Van Hulle <stijn@stijnvanhulle.be>
2022-06-14 12:30:18 +0000

    feat: rustraf cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,32 @@
-console.log("Not implemented yet");
+#!/usr/bin/env node
+
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const { removeFile } = require("../index");
+
+const commands = hideBin(process.argv);
+
+const builder = yargs(process.argv.slice(2))
+  .scriptName("rustraf")
+  .command(
+    "$0",
+    "rustraf <path> [<path> ...]",
+    () => {},
+    () => {
+      const [file] = commands;
+      try {
+        removeFile(file);
+      } catch (e) {
+        console.error("Could not delete ", file);
+      }
+    }
+  )
+  .showHelpOnFail(true)
+  .demandCommand(1)
+  .version();
+
+if (commands.length === 0) {
+  builder.showHelp().parse();
+} else {
+  builder.parse();
+}
